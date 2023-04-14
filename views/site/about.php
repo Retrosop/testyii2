a7be7d0c2deb151de5393f5f20430bd2f406574a
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-04-14 10:32:10 +1000

    test1

---
+++ views/site/about.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var yii\web\View $this */
+
+use yii\helpers\Html;
+
+$this->title = 'About';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+   
+   
+  
+   <input type='button' value='Задание 2' onclick='messageWrite2()' /> <br>
+   
+   <button id=replaceoradd>Мой первый jquery</button>
+   
+   <div id=rezultat>
+   
+   </div>
+  
+</div>
